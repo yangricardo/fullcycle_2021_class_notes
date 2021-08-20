644c79ddb06c2e1d43d37667f46128cf664bd6ff
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2021-08-20 20:19:14 -0300

    modulo kubernetes

---
+++ modulo_kubernetes/2_primeiros_passos_na_pratica/README.md
@@ -0,0 +1,9 @@
+# Primeiros passos na prática
+
+- `go mod init hello/m`: inicia o modulo hello, geral o arquivo go.mod
+
+- `go build -o server .`: gera binário do servidor
+
+- `docker build -t yangricardo/hello-go .`: gera imagem docker
+
+- `docker run --rm -p 3080:80`
--- modulo_kubernetes/2_primeiros_passos_na_pratica/server.go
+++ modulo_kubernetes/2_primeiros_passos_na_pratica/server.go
@@ -1,12 +1,15 @@
-package primeirospassosnapratica
+package hello
 
 import "net/http"
 
 func main() {
+	// Mapeia função Hello para a rota principal
 	http.HandleFunc("/", Hello)
+	// monitora requisições na porta 80
 	http.ListenAndServe(":80", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
+	// retorna os bytes da mensagem
 	w.Write([]byte("<h1>Hello</h1>"))
 }
